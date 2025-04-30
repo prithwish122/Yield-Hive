b3fb7d9c8abf1944f99caea34b54d4a15212b6e5
prithwish122 <prithwishchatterjee1277@gmail.com>
2025-04-30 22:59:00 +0530

    wallet added

--- app/context/index.tsx
+++ app/context/index.tsx
@@ -5,7 +5,7 @@
 import { wagmiAdapter, projectId } from '../config'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createAppKit } from '@reown/appkit/react' 
-import { mainnet, arbitrum, avalanche, base, optimism, polygon } from '@reown/appkit/networks'
+import { mainnet, arbitrum, avalanche, base, optimism, polygon, defineChain } from '@reown/appkit/networks'
 import React, { type ReactNode } from 'react'
 import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
 
@@ -24,16 +24,40 @@ const metadata = {
   icons: ['https://assets.reown.com/reown-profile-pic.png']
 }
 
+const customNetwork = defineChain({
+  id: 50002,
+  caipNetworkId: 'eip155:123456789',
+  chainNamespace: 'eip155',
+  name: 'Pharos Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Pharos',
+    symbol: 'PS',
+  },
+  rpcUrls: {
+    default: {
+      http: ['http://devnet.dplabs-internal.com'],
+      webSocket: ['WS_RPC_URL'],
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Explorer', url: 'BLOCK_EXPLORER_URL' },
+  },
+  contracts: {
+    // Add the contracts here
+  }
+})
+
+
+
 // Create the modal
 const modal = createAppKit({
-  adapters: [wagmiAdapter],
-  projectId,
-  networks: [mainnet, arbitrum, avalanche, base, optimism, polygon],
-  defaultNetwork: mainnet,
-  metadata: metadata,
-  features: {
-    analytics: true, // Optional - defaults to your Cloud configuration
-  }
+  adapters: [wagmiAdapter], // Add valid adapter objects here if needed
+  networks: [customNetwork],
+  chainImages: {
+    123456789: '/custom-network-logo.png',
+  },
+  projectId: '4327a606012c25c0f109a20cda59f606'
 })
 
 function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
